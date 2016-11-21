f09b859a927f82ab506a48738347c9903afbd535
NewDelion <el.emena.crea@gmail.com>
2016-11-21 19:50:12 +0900

    Init

---
+++ AmenixEditer/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace AmenixEditer
+{
+    public partial class MainWindow : MetroWindow
+    {
+        private ViewModel.MainViewModel vm = new ViewModel.MainViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        #region //Initialize
+        private void init()
+        {
+            DataContext = vm;
+            textEditor.TextArea.Caret.PositionChanged += (sender, e) =>
+            {
+                vm.Column = textEditor.TextArea.Caret.Column;
+                vm.Line = textEditor.TextArea.Caret.Line;
+            };
+            install_FindReplace();
+
+            textEditor.Focus();
+        }
+
+        private FindReplace.FindReplaceMgr FRManager = null;
+        private void install_FindReplace()
+        {
+            FRManager = new FindReplace.FindReplaceMgr();
+            FRManager.CurrentEditor = new FindReplace.TextEditorAdapter(textEditor);
+            FRManager.ShowSearchIn = false;
+            FRManager.OwnerWindow = this;
+            CommandBindings.Add(FRManager.FindBinding);
+            CommandBindings.Add(FRManager.ReplaceBinding);
+            CommandBindings.Add(FRManager.FindNextBinding);
+        }
+        #endregion
+    }
+
+    /*
+    http://sourcechord.hatenablog.com/entry/2016/02/23/012511
+    http://qiita.com/kabosu/items/11fa400bdd82395f59fb
+
+    */
+}
---
+++ AmenixEditer/ViewModel/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace AmenixEditer.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private int _column = 1;
+        public int Column
+        {
+            get { return _column; }
+            set
+            {
+                _column = value;
+                NotifyPropertyChanged(nameof(Column));
+            }
+        }
+
+        private int _line = 1;
+        public int Line
+        {
+            get { return _line; }
+            set
+            {
+                _line = value;
+                NotifyPropertyChanged(nameof(Line));
+            }
+        }
+
+
+    }
+}
